baeaa8614c50c65b381330a1e37d4b5a8e7da870
SalarUW <salarahmaduw@gmail.com>
2018-08-03 19:14:12 -0400

    First commit for this project

---
+++ src/main/java/com/salar/wattodo/wattodo/WattodoApplication.java
@@ -0,0 +1,12 @@
+package com.salar.wattodo.wattodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattodoApplication.class, args);
+	}
+}
